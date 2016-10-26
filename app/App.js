d32726ae37a91799657ea423ef15d780825a5e70
thedanflan <thedanflan@gmail.com>
2016-10-26 01:30:39 -0500

    first commit

--- app/App.js
+++ app/App.js
@@ -8,9 +8,11 @@ export default class App extends React.Component {
   }
   render() {
     return (
+      <div>
       <div className={styles.app}>
-        bar
+      <
       </div>
-    );
+      
+    }
+
   }
-}
--- server.js
+++ server.js
@@ -12,27 +12,17 @@ const port = isDeveloping ? 3000 : process.env.PORT;
 const app = express();
 
 if (isDeveloping) {
-  const compiler = webpack(config);
-  const middleware = webpackMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-    contentBase: 'src',
-    stats: {
-      colors: true,
-      hash: false,
-      timings: true,
-      chunks: false,
-      chunkModules: false,
-      modules: false
-    }
-  });
 
-  app.use(middleware);
-  app.use(webpackHotMiddleware(compiler));
-  app.get('*', function response(req, res) {
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
-    res.end();
-  });
-} else {
+  app.use(express.static(__dirname + '/app'));
+
+app.get('*', function response(req, res) {
+  res.json({foo:'bar'});  
+});
+app.post('*', function response(req, res) {
+  res.json({success:'true'});  
+});
+}
+ else {
   app.use(express.static(__dirname + '/dist'));
   app.get('*', function response(req, res) {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
