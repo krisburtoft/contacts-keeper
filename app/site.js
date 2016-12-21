5a7e49fab36b90980460b99dc2b4d0bc73a1547b
thedanflan <thedanflan@gmail.com>
2016-12-20 20:24:02 -0600

    B4 Bootstrap

--- app/site.js
+++ app/site.js
@@ -24,11 +24,23 @@ var search = $('input[name="search"]');
 
 var update =$('#update');
 
-var serbut = $('#serbut');
-
 var edit = $('#edit');
 
-var sort = $('#sort');
+var del = $('#delete');
+
+var mySort = $('#mySort');
+
+var sortDropDown =$('#sortDropDown')
+
+var sortBy = $('#sortBy');
+
+var A_Z = $('#A_Z')
+
+var Z_A = $('#Z_A')
+
+var one_9 = $('#one_9')
+
+var nine_1 = $('#nine_1')
 
 function time() {
 var dt = new Date();
@@ -37,6 +49,7 @@ return (dt.getYear() + ":" + dt.getMonth() + ":" + dt.getDay() + ":" + dt.getHou
 
 btn.on('click',function() {
     modal.show();
+    mySort.hide();
     $("input[type=text]").val("");
 })
 
@@ -91,10 +104,10 @@ function getContacts() {
 	return JSON.parse(retrieveContacts);
 }
 
-
 function createEditHandler(contact) {
 	return function editHandler() {
 		modal.show();
+		mySort.hide();
 		fname.val(contact.fname)
 		lname.val(contact.lname)
 		dob.val(contact.dob)
@@ -125,43 +138,112 @@ function createUpdateHandler(contact) {
 		modal.hide();
 		update.hide();
 		save.show();
-		
 	}
 }
+
+del.on('click', function createRemainderArray(contact) {
+	function singleOutContact() {
+		var contacts = getContacts();
+		var oldContacts = contacts.filter(function (oldContact) {
+		return oldContact.TS != contact.TS;
+		console.log(oldContacts);
+		});
+	}
+});
 
 function getContactArray() {
 	return getContacts();
 }
 
 function renderContactRow(contact) {
-	
 	var tr = $('<tr data-id="'+ contact.TS +'"><td>' + contact.fname + '</td>' + 
 		'<td>' + contact.lname + '</td>' + 
 		'<td>' + contact.dob + '</td>' + 
 		'<td>' + contact.pnumber + '</td>' +
 		'<td>' + contact.email + '</td>' +
-		'<td>' + contact.notes + '</td></tr>');
+		'<td>' + contact.notes + '</td>' + '<td>' + 
+		'<button id="edit" data-id="'+ contact.TS +'">' + "edit" + '</button>' +
+		'<button id="delete" data-id="'+ contact.TS +'">' + "delete" + '</button>' + '</td></tr>' );
 		$('table').append(tr);
 		tr.dblclick(createEditHandler(contact));
 }
 
-sort.on('click', function sortArray(event) {
+sortBy.on('click', function showSort() {
+	mySort.fadeIn();
+	sortDropDown.show();
+})
+
+A_Z.on('click', function sortArray(event) {
 	event.preventDefault();
 	$('tbody tr').remove();
 	sortFname().forEach(renderContactRow);
+	A_Z.hide();
+	Z_A.show();	
+})
+
+Z_A.on('click', function sortArray(event) {
+	event.preventDefault();
+	$('tbody tr').remove();
+	sortFnameRev().forEach(renderContactRow);
+	Z_A.hide();
+	A_Z.show();	
+})
+
+one_9.on('click', function sortArray(event) {
+	event.preventDefault();
+	$('tbody tr').remove();
+	sortPnumber().forEach(renderContactRow);
+	one_9.hide();
+	nine_1.show();	
+})
+
+nine_1.on('click', function sortArray(event) {
+	event.preventDefault();
+	$('tbody tr').remove();
+	sortPnumberRev().forEach(renderContactRow);
+	nine_1.hide();
+	one_9.show();	
 })
 
 function sortFname() {
 	return createFnameArray;
 }
 
+function sortFnameRev() {
+	return createFnameArrayRev;
+}
+
 function sortLname() {
 	return createLnameArray;
 }
 
+function sortLnameRev() {
+	return createLnameArrayRev;
+}
+
+function sortPnumber() {
+	return createPnumberArray;
+}
+
+function sortPnumberRev() {
+	return createPnumberArrayRev;
+}
+
 var createFnameArray = getContactArray().sort(function(a,b)  {return (a.fname > b.fname) ? 1 : ((b.fname > a.fname) ? -1 : 0);} ); 
 
+var createFnameArrayRev = getContactArray().sort(function(a,b)  {return (a.fname < b.fname) ? 1 : ((b.fname < a.fname) ? -1 : 0);} ); 
+
 var createLnameArray = getContactArray().sort(function(a,b) {return (a.lname > b.lname) ? 1 : ((b.lname > a.lname) ? -1 : 0);} ); 
+
+var createLnameArrayRev = getContactArray().sort(function(a,b) {return (a.lname < b.lname) ? 1 : ((b.lname < a.lname) ? -1 : 0);} ); 
+
+var createPnumberArray = getContacts().sort(function(a, b) {
+    return parseFloat(a.pnumber) - parseFloat(b.pnumber);
+});
+
+var createPnumberArrayRev = getContacts().sort(function(a, b) {
+    return parseFloat(b.pnumber) - parseFloat(a.pnumber);
+});
 
 function checkContacts(contact) {
 	var searchStr = search.val().toLowerCase();
@@ -171,8 +253,6 @@ function checkContacts(contact) {
 	contact.pnumber.toLowerCase().indexOf(searchStr) >-1 ||
 	contact.email.toLowerCase().indexOf(searchStr) >-1 ||
 	contact.notes.toLowerCase().indexOf(searchStr) >-1;
-
-	// console.log(isMatch, contact);
 	return isMatch;
 }
 
@@ -188,6 +268,3 @@ getContactArray().forEach(renderContactRow);
 }
 
 $(document).ready(renderContactRows);
-
-
-
